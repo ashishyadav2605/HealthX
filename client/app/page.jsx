294ef628cd539c1ddc5807b4c741fa14cb1e3f2e
ashishyadav2605 <iashishyadav17@gmail.com>
2025-08-07 10:56:34 +0530

    done

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,16 +1,16 @@
 'use client';
 
-import Header from '../components/Header';
-import HeroSection from '../components/HeroSection';
-import FeaturesSection from '../components/FeaturesSection';
-import NetworkFlowAnimation from '../components/NetworkFlowAnimation';
-import SeamlessAccess from '../components/SeamlessAccess';
-import HassleFreeLX from '../components/HassleFreeLX';
-import ABHASteps from '../components/ABHASteps';
-import Feedbacks from '../components/Feedbacks';
-import PrivacyNote from '../components/PrivacyNote';
-import Newsletter from '../components/Newsletter';
-import Footer from '../components/Footer';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import FeaturesSection from './components/FeaturesSection';
+import NetworkFlowAnimation from './components/NetworkFlowAnimation';
+import SeamlessAccess from './components/SeamlessAccess';
+import HassleFreeLX from './components/HassleFreeLX';
+import ABHASteps from './components/ABHASteps';
+import Feedbacks from './components/Feedbacks';
+import PrivacyNote from './components/PrivacyNote';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
